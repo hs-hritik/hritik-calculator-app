8fa61f9e69a7893067642ee4fcc2dc3c2702dfbb
VaibhavKakade <vaibhavkakade@helpshift.com>
2017-10-03 14:55:06 +0530

    Restore api data after reset
    Change-Id: I473427739bb1c5950d688746322d1ba75d050373

--- resources/static/scripts/reducers/root.js
+++ resources/static/scripts/reducers/root.js
@@ -18,14 +18,33 @@ define ("reducers/root",
     faqViewReducer, csatViewReducer, ACTION_TYPES) {
     "use strict";
 
+    const update = React.addons.update;
+
+    /**
+     * Return restored state after adding api data to clean state
+     * @param {Object} oldState - Old state
+     * @param {Object} newState - New state
+     * @returns - Restored state containing api data
+     */
+    const restoreApiData = (oldState, newState) => {
+      const {cif} = oldState.appState;
+      return update (newState, {
+        appState: {
+          cif: {$set: cif}
+        }
+      });
+    };
+
     const enableBatching = (reducer) => {
       const batchingReducer = (state, action) => {
         switch (action.type) {
           case ACTION_TYPES.BATCH_ACTIONS:
             return action.actions.reduce (batchingReducer, state);
 
+          // @TODO :- Reconsider this approach. Handle RESET action in each reducer
           case ACTION_TYPES.RESET:
-            return reducer (undefined, action);
+            const newState = reducer (undefined, action);
+            return restoreApiData (state, newState);
 
           default:
             return reducer (state, action);
