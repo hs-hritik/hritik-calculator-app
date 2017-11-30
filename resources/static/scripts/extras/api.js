3377ea3c10b8e869a39e64ea22ce424c8bf941a5
VaibhavKakade <vaibhavkakade@helpshift.com>
2017-11-30 16:15:54 +0530

    Handle CSAT rating submission on widget minimize
    Change-Id: Iabd0d522d8f6a867900316f75a302aedb8927ada

--- resources/static/scripts/extras/api.js
+++ resources/static/scripts/extras/api.js
@@ -15,11 +15,12 @@ define ("extras/api",
     "actions/chatView",
     "actions/businessHours",
     "actions/actionCreators",
+    "actions/csatView",
     "components/app"
   ],
   function (store, EVENT_TYPES, APP_STATE_CONSTANTS, ACTIVE_VIEW,
     postSdkMessage, appStateActions, chatViewActions, businessHoursActions,
-    actionCreators, app) {
+    actionCreators, csatViewActions, app) {
     "use strict";
 
     const {ISSUE_STATE, PRE_CHAT_STATE, PRE_CHAT_FEATURES} = APP_STATE_CONSTANTS;
@@ -47,6 +48,18 @@ define ("extras/api",
      */
     const isIssueClosed = (issueState) => {
       return ISSUE_CLOSED_STATES.indexOf (issueState) !== -1;
+    };
+
+    /**
+     * Handle CSAT rating submission
+     */
+    const handleCsatRatingSubmission = () => {
+      // If current view is CSAT view and user has not submitted the rating,
+      // submit the rating on minimize
+      const {appState, csatView} = store.getState ();
+      if (appState.activeView === ACTIVE_VIEW.CSAT && !csatView.completed) {
+        store.dispatch (csatViewActions.submitCsat ());
+      }
     };
 
     /**
@@ -86,6 +99,7 @@ define ("extras/api",
         }
 
       } else if (isIssueClosed (appState.issueState)) {
+        handleCsatRatingSubmission ();
         // If minimized is true, and issue state is closed, reset the conversation.
         store.dispatch (appStateActions.reset ({
           skipUser: true
