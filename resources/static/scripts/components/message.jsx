a7066c2d5911da780cd7054cc76a77be72b85527
VaibhavKakade <vaibhavkakade@helpshift.com>
2017-11-30 15:49:53 +0530

    Change csat message layout
    Change-Id: Id5877227a8fc90e8820a9b6f55a849a0f8afe3dc

--- resources/static/scripts/components/message.jsx
+++ resources/static/scripts/components/message.jsx
@@ -214,19 +214,9 @@ define ("components/message",
        * Render csat request message.
        */
       _renderCsatMessage () {
-        const {text} = this.props;
-
-        // @TODO: Rename classes.
         return (
           <div className="hs-message__item">
-            {text.csatBotRequestMsg}
-            <div className="hs-message__suggested-faqs">
-              <a className="hs-message__suggested-faq"
-                 onClick={this.props.onStartCsatSurveyClick}>
-                {text.csatLinkCaption}
-                <i className="ion-chevron-right hs-message__suggested-faq-icon" />
-              </a>
-            </div>
+            {this.props.text.csatBotRequestMsg}
           </div>
         );
       },
