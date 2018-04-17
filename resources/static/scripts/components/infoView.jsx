d7ff616f2089b79f3c30175e2a8d292997868030
Manish Garg <manish@helpshift.com>
2018-04-17 17:46:16 +0530

    Update InfoView component to show errors
    Change-Id: Id3e117082af867d9701b1b8315410627bb941412

--- resources/static/scripts/components/infoView.jsx
+++ resources/static/scripts/components/infoView.jsx
@@ -17,13 +17,69 @@ define ("components/infoView",
     return React.createClass ({
       displayName: "InfoView",
       propTypes: {
-        loading: PropTypes.bool
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        loading: PropTypes.bool,
+        actionBtnText: PropTypes.string,
+        onActionBtnClick: PropTypes.func
       },
 
       render () {
         return (
           <div className="hs-info-view">
+            {this._renderTitle ()}
+            {this._renderSubtitle ()}
+            {this._renderRetryBtn ()}
             {this._renderLoader ()}
+          </div>
+        );
+      },
+
+      _renderTitle () {
+        const {title} = this.props;
+        if (!title) {
+          return null;
+        }
+
+        return (
+          <div className="hs-info-view__title">
+            {title}
+          </div>
+        );
+      },
+
+      _renderSubtitle () {
+        const {subtitle} = this.props;
+        if (!subtitle) {
+          return null;
+        }
+
+        return (
+          <div className="hs-info-view__subtitle">
+            {subtitle}
+          </div>
+        );
+      },
+
+      _renderRetryBtn () {
+        const {actionBtnText} = this.props;
+        if (!actionBtnText) {
+          return null;
+        }
+
+        // @TODO: Move hs-chat-footer__button and hs-chat-footer__buttons-wrapper
+        // to some common place because this will be used across most buttons in web chat.
+        const btnClasses = classes (
+          "hs-button",
+          "hs-button--hollow",
+          "hs-chat-footer__button"
+        );
+
+        return (
+          <div className="hs-chat-footer__buttons-wrapper">
+            <button className={btnClasses} onClick={this.props.onActionBtnClick}>
+              {actionBtnText}
+            </button>
           </div>
         );
       },
