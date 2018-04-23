a0e20399adc73ef4d58b248a94a2dfc909d9de67
VaibhavKakade <vaibhavkakade@helpshift.com>
2018-04-23 20:55:53 +0530

    Fix chrome box shadow issue
    Change-Id: Id0f97d53397b3361d782f4dce9e04cfbde682c52

--- resources/static/scripts/external/messenger.js
+++ resources/static/scripts/external/messenger.js
@@ -126,6 +126,7 @@
     "border-radius": "8px",
     "z-index": "9999999",
     "overflow":"hidden",
+    "transform": "translate3d(0,0,0)",
     "box-shadow": "0 4px 32px rgba(0, 0, 0, .2)"
   };
 
