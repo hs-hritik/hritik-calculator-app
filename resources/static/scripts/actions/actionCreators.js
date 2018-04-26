9cee5c333139c245737a8af6f42d01713f8c8411
VaibhavKakade <vaibhavkakade@helpshift.com>
2018-04-26 11:50:37 +0530

    Clean up : action creators
    Change-Id: Ifb53a7fe6cc5b6bae4c008366378f11db4e4cf24

--- resources/static/scripts/actions/actionCreators.js
+++ resources/static/scripts/actions/actionCreators.js
@@ -100,30 +100,6 @@ define ("actions/actionCreators",
     });
 
     /**
-     * Return the action to set prechat feature index
-     * @param {number} index - the prechat feature index to set
-     * @returns {Object} - the action object
-     */
-    const setPreChatFeatureIndex = (index) => {
-      return {
-        type: ACTION_TYPES.SET_PRE_CHAT_FEATURE_INDEX,
-        preChatFeatureIndex: index
-      };
-    };
-
-    /**
-     * Return the action to set executeGreetingMessage in app state
-     * @param {boolean} executeGreetingMessagex - set the value to true or false
-     * @returns {Object} - the action object
-     */
-    const setExecuteGreetingMessage = (executeGreetingMessage) => {
-      return {
-        type: ACTION_TYPES.SET_EXECUTE_GREETING_MESSAGE,
-        executeGreetingMessage
-      };
-    };
-
-    /**
      * Return the action to set if the suggested FAQ read event has been tracked.
      * @param {boolean} isTracked
      * @returns {Object}
@@ -157,18 +133,6 @@ define ("actions/actionCreators",
       return {
         type: ACTION_TYPES.SET_INTERNAL_ISSUE_ID,
         id
-      };
-    };
-
-    /**
-     * Return the action to set the timestamp when the info bot gets requested.
-     * @param {string} number
-     * @returns {Object}
-     */
-    const setInfoBotRequestedTimestamp = (ts) => {
-      return {
-        type: ACTION_TYPES.SET_INFO_BOT_REQESTED_TIMESTAMP,
-        ts
       };
     };
 
@@ -240,12 +204,9 @@ define ("actions/actionCreators",
       setLanguage,
       setCif,
       setTags,
-      setPreChatFeatureIndex,
-      setExecuteGreetingMessage,
       setSuggestedFaqReadTracked,
       updateReadFaqList,
       setInternalIssueId,
-      setInfoBotRequestedTimestamp,
       setResolutionQuestionCompleted,
       setCsatCompleted,
       setFullPrivacy,
