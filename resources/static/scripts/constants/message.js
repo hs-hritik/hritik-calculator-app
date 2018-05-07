582a08f1585b7139b05e7140ee8253b694594550
VaibhavKakade <vaibhavkakade@helpshift.com>
2018-05-07 12:54:31 +0530

    Clean up : message constants
    Change-Id: Ic6fe73456721599cdcb978a04793d069388e28ed

--- resources/static/scripts/constants/message.js
+++ resources/static/scripts/constants/message.js
@@ -100,19 +100,6 @@ define ("constants/message",
       SOLUTION_REJECTED: "Did not accept the solution"
     };
 
-    // Typing timeout (in milliseconds) for different system generated message.
-    const TYPING_TIMEOUT = {
-      FAQ_SUGGESTIONS_ADDITIONAL_HELP: 1200,
-      INFO_BOT_REQUEST: 1200,
-      INFO_BOT_FIELD: 1200,
-      FAQ_SUGGESTIONS_PROBLEM_SOLVED: 1200
-    };
-
-    // Timeout (in milliseconds) for different system generated message.
-    const TIMEOUT = {
-      FAQ_SUGGESTIONS_ADDITIONAL_HELP: 600
-    };
-
     const ORIGIN = {
       ADMIN: "admin"
     };
@@ -123,8 +110,6 @@ define ("constants/message",
 
     return {
       TYPE,
-      TIMEOUT,
-      TYPING_TIMEOUT,
       ORIGIN,
       STATE,
       BODY,
