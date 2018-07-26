2871f76d5adb620c1ec312a079497d4cb78b7a5d
Shubham Jain <shubham@helpshift.com>
2018-07-26 11:06:04 +0530

    Fix: Messages seen PUT call getting triggered before issue is created
    Change-Id: If04a114ce771260c39c6d3cda5b24ede4609678b

--- resources/static/scripts/extras/api.js
+++ resources/static/scripts/extras/api.js
@@ -133,7 +133,7 @@ define ("extras/api",
           issueState
         },
         chatView: {
-          unreadCount
+          unreadMessageIds
         }
       } = store.getState ();
 
@@ -142,9 +142,9 @@ define ("extras/api",
           app.init ();
         }
 
-        // If unreadCount isn't zero and active view is chat view,
+        // If unread count isn't zero and active view is chat view,
         // dispatch action to mark messages seen.
-        if (unreadCount !== 0 && ACTIVE_VIEW.CHAT === activeView) {
+        if (unreadMessageIds.length !== 0 && ACTIVE_VIEW.CHAT === activeView) {
           store.dispatch (chatViewActions.markMessagesSeen ());
         }
 
