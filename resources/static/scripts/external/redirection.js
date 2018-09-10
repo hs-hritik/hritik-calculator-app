93f2435ad9e57e2ba0c56ee1bf98339177a45fa4
aniketh <aniket@helpshift.com>
2018-09-10 16:50:52 +0530

    Add JS file for redirection page
    patch update
    
    Change-Id: I609cf972374f3a53274227eadbd821336285eecd
    JIRA: https://helpshift.atlassian.net/browse/FRON-3437

---
+++ resources/static/scripts/external/redirection.js
@@ -0,0 +1,11 @@
+/**
+ * JS for redirection page
+ * @author Aniket Hendre <aniket@helpshift.com>
+ * @created Sep 10, 2018
+ */
+
+(function () {
+  "use strict";
+
+  // @TODO: Add redirection related JS code
+}) ();
