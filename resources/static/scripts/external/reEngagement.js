d7078333353a9a63b5d959804c12f1b2be652abe
aniketh <aniket@helpshift.com>
2018-09-10 17:20:41 +0530

    Add JS file for re-engagement page
    patch update
    
    Change-Id: If79dddac134fa71e259115d5a3c6ef801c97faee
    JIRA: https://helpshift.atlassian.net/browse/FRON-3437

---
+++ resources/static/scripts/external/reEngagement.js
@@ -0,0 +1,11 @@
+/**
+ * JS for re-engagement page
+ * @author Aniket Hendre <aniket@helpshift.com>
+ * @created Sep 10, 2018
+ */
+
+(function () {
+  "use strict";
+
+  // @TODO: Implement re-engagement related JS code
+}) ();
