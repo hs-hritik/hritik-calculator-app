ff704fe3c0aba119d809fe12607eedc8dee2d58e
aniketh <aniket@helpshift.com>
2018-09-11 15:16:19 +0530

    Create XHR to get the re-engagement config
    - Get pid from XHR response & pass it to create an iframe
      corresponding to pid.hs.com
    
    patch update
    
    Change-Id: I3157d7a6ac0bb98dac1148a4052c9f6add362b44
    JIRA: https://helpshift.atlassian.net/browse/FRON-3437

--- resources/static/scripts/external/redirection.js
+++ resources/static/scripts/external/redirection.js
@@ -1,5 +1,10 @@
 /**
- * JS for redirection page
+ * JS for getting the user config from backend and depending
+ * on data creating an iframe for the respective pid.hs.com
+ * The purpose of all of this is to set the appropriate values
+ * from user's previous session into the local storage so that
+ * user can continue the conversation.
+ *
  * @author Aniket Hendre <aniket@helpshift.com>
  * @created Sep 10, 2018
  */
@@ -7,24 +12,117 @@
 (function () {
   "use strict";
 
+  const GET_USER_CONFIG_URL = "/websdk/get-user-config/";
+
+  /**
+   * Convert passed object to query string format
+   * @param {Object} obj - data for the XHR
+   * @returns {String} - query string
+   */
+  const queryStringify = (obj) => {
+    const qs = [];
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty (key)) {
+        qs.push (encodeURIComponent (key) + "=" + encodeURIComponent (obj [key]));
+      }
+    }
+    return qs.join ("&");
+  };
+
+  /**
+   * Function to make an XMLHttpRequest.
+   * This method only supports the GET request as it's the only
+   * type of XHR required on this page.
+   * @param {params} Object - object holding XHR parameters
+   * @param {params.url} String - URL for the XHR
+   * @param {params.data} Object - Object holding that needs to be sent as payload
+   * @param {params.onSucess} Function - Callback function to call when xhr is
+   * successful.
+   * @returns {XMLHttpRequest Object} xhr
+   */
+  const sendXhr = (params = {}) => {
+    const xhr = new XMLHttpRequest ();
+    let {url} = params;
+    const {data, onSuccess} = params;
+    const method = "GET";
+
+    if (data) {
+      url += "?" + queryStringify (data);
+    }
+
+    xhr.open (method, url, true);
+    xhr.setRequestHeader ("X-Requested-With", "XMLHttpRequest");
+
+    // Handling success callback only as the XHR is expected to fail silently
+    xhr.onreadystatechange = function () {
+      if ((xhr.readyState === 4) &&
+          (xhr.status >= 200 && xhr.status < 300) &&
+          onSuccess) {
+        onSuccess (JSON.parse (xhr.responseText), xhr, xhr.status);
+      }
+    };
+
+    xhr.send ();
+    return xhr;
+  };
+
+  /**
+   * Fire XHR to get the re-engagement config.
+   * @param {String} link - Re-engagement link
+   * @param {Function} onSuccess - Function to execute on success of XHR
+   * @retruns {Object} XMLHttpRequest
+   */
+  const getReEnagementConfig = (link, onSuccess) => {
+    return sendXhr ({
+      GET_USER_CONFIG_URL,
+      data: {
+        link
+      },
+      onSuccess
+    });
+  };
+
   /**
    * Create iframe for pid.hs.com
+   * @param {String} src - Source of the iframe
    * @returns {Element} - re-engagement iframe
    */
-  var createReEngagementIframe = function () {
+  const createReEngagementIframe = (src) => {
     const iframe = document.createElement ("iframe");
 
     iframe.id = "hs-re-engagement-iframe";
-    // @TODO: Replace this with correct re-engagement.html path
-    iframe.src = "/html/re-engagement.html";
+    iframe.src = src;
     iframe.style.display = "none";
     return iframe;
   };
 
   /*
-   * Create & append iframe to the body
+   * Get re-engagement config from backend & on success of the XHR
+   * create & append re-engagement iframe to the body.
    */
-  const iframe = createReEngagementIframe ();
-  document.body.appendChild (iframe);
+  getReEnagementConfig (window.location.href, (response) => {
+    // On dev env, this gets replaced by a localhost URL.
+    // See babel tasks in resources/gulp/javascript.js
+    const WEB_CHAT_ROOT = "{{ENV_WEB_CHAT_ROOT}}";
+    const urlParts = WEB_CHAT_ROOT.split ("://"),
+          PROTOCOL = `${urlParts [0]}://`,
+          PLAT_ID = response.pid,
+          HOST = urlParts [1],
+          PATH = "/html/re-engagement.html";
 
+    // Truncate platform id to a fixed length (24 in this implementation).
+    // Here's an example platform id - testdomain_platform_20170901110844149-0319dffe2b25f9c
+    // Part 1 - First split plat id by "_platform_" and slice the first part by 8
+    // chars -> get the first 8 chars of the domain. "testdoma" in this case.
+    // Part 2 - Then slice the plat id from the end by 16 chars -> get a unique
+    // part of the platform id. "-0319dffe2b25f9c" in this case.
+    const TRUNCATED_PLAT_ID = PLAT_ID.split ("_platform_") [0].slice (0, 8) + PLAT_ID.slice (-16);
+
+    const DOMAIN = `${PROTOCOL}${TRUNCATED_PLAT_ID}.${HOST}`;
+    const IFRAME_SRC = `${DOMAIN}${PATH}`;
+
+    const iframe = createReEngagementIframe (IFRAME_SRC);
+    document.body.appendChild (iframe);
+  });
 }) ();
