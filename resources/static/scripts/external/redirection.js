9f77a191f7e8381f1fce14e26a618b67c7e4d369
aniketh <aniket@helpshift.com>
2018-09-11 12:52:31 +0530

    Add function to create re-engagement iframe
    Change-Id: I9cafc1cbca8d82f9914803fd07fca39d123bdd68
    JIRA: https://helpshift.atlassian.net/browse/FRON-3437

--- resources/static/scripts/external/redirection.js
+++ resources/static/scripts/external/redirection.js
@@ -7,5 +7,24 @@
 (function () {
   "use strict";
 
-  // @TODO: Add redirection related JS code
+  /**
+   * Create iframe for pid.hs.com
+   * @returns {Element} - re-engagement iframe
+   */
+  var createReEngagementIframe = function () {
+    const iframe = document.createElement ("iframe");
+
+    iframe.id = "hs-re-engagement-iframe";
+    // @TODO: Replace this with correct re-engagement.html path
+    iframe.src = "/html/re-engagement.html";
+    iframe.style.display = "none";
+    return iframe;
+  };
+
+  /*
+   * Create & append iframe to the body
+   */
+  const iframe = createReEngagementIframe ();
+  document.body.appendChild (iframe);
+
 }) ();
