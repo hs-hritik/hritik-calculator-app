8b4a33a23d03671c1ca503e9a03c5dbc419cd8dd
aniketh <aniket@helpshift.com>
2018-10-28 13:38:49 +0530

    Change condition to start the poller if issue exists
    Start the poller if XHR fails with 400 & error message
    "EXISTING_ACTIVE_CONVERSATION"
    
    Change-Id: I0ef3b43c5658fb54bf40277e8a2977771e8d370a
    JIRA: https://helpshift.atlassian.net/browse/FRON-3533

--- resources/static/scripts/actions/chatView.js
+++ resources/static/scripts/actions/chatView.js
@@ -2026,13 +2026,7 @@ define ("actions/chatView",
           data: xhrHelpers.getPreparedXhrData (xhrData),
           headers: xhrHelpers.getCommonHeaders (),
           method: "POST",
-          onSuccess: (response, xhrObj, statusCode) => {
-            // If pre-issue exists then just start the poller to fetch existing.
-            if (statusCode === RESPONSE_STATUS_CODE.PRE_ISSUE_EXISTS) {
-              startPollingForMessages ();
-              return;
-            }
-
+          onSuccess: (response) => {
             const newIssueId = response.id;
             const internalId = response.type === ISSUE_TYPE.PRE_ISSUE ?
               response.preissue_id : response.issue_id;
@@ -2052,7 +2046,14 @@ define ("actions/chatView",
             // @TODO: Confirm if issue created event has to be tracked from Web Chat.
             // analyticsHelpers.track (EVENT.ISSUE_CREATED);
           },
-          onFailure: () => {
+          onFailure: (response, xhrObj, statusCode) => {
+            // If pre-issue exists then just start the poller to fetch existing.
+            if (statusCode === RESPONSE_STATUS_CODE.BAD_REQUEST &&
+                response.error === ERROR_TYPES.EXISTING_ACTIVE_CONVERSATION) {
+              startPollingForMessages ();
+              return;
+            }
+
             dispatch (batchActions ([
               setChatViewError ({
                 type: ERROR_TYPES.PRE_ISSUE_FAILURE,
