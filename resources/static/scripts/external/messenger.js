e86f8c13549272a330796708bd4457b926bc3ff0
Prasenjit Sharan <prasenjit@helpshift.com>
2018-11-06 10:02:02 +0530

    Stop using "*" as targetOrigin with postMessage
    postMessage from web chat -> parent uses "*" for the first message it
    passes to the parent (sdk_js_loaded). Subsquent calls use the parent
    page origin stored in the state. We want to avoid using "*" even for the
    first call. This is done by passing the parent origin as a query param
    to the web chat iframe's URL and then reading it in the web chat app
    before calling all postMessage requests from web chat -> parent.
    
    Change-Id: Ifddde5108d11d5f49ede4dc071932a6e81c5a0b7

--- resources/static/scripts/external/messenger.js
+++ resources/static/scripts/external/messenger.js
@@ -29,7 +29,14 @@
 
   // @TODO: Rename WEB_SDK -> WEB_CHAT
   const WEB_SDK_DOMAIN = `${PROTOCOL}${TRUNCATED_PLAT_ID}.${HOST}`;
-  const WEB_SDK_URL = `${WEB_SDK_DOMAIN}${PATH}`;
+
+  // A query string with parent page's origin, which is later used with the
+  // postMessage call by web chat to the parent page.
+  const PARENT_ORIGIN_QUERY_STRING = `parent=${win.location.origin}`;
+
+  // @TODO: Use `&` or `?` appropriately. PATH already contains hard-coded `?` so
+  // it's safe to use `&` here but this must be made generic when `?` is removed.
+  const WEB_SDK_URL = `${WEB_SDK_DOMAIN}${PATH}&${PARENT_ORIGIN_QUERY_STRING}`;
 
   const WIDGET_POSITIONS = {
     TOP_LEFT: "top-left",
