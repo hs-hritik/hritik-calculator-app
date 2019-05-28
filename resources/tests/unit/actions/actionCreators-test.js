b21bb7c744f839d8319d4f2917e100d108959eb3
Prasenjit Sharan <prasenjit@helpshift.com>
2019-05-28 11:57:08 +0530

    Set up Jest and write initial tests
    Change-Id: I9c278c953b13320e6dce7992f0c64b5800ae1188

---
+++ resources/tests/unit/actions/actionCreators-test.js
@@ -0,0 +1,28 @@
+// @TODO: This is a CommonJS module. Explore how to write ES6 modules for tests.
+const actionCreators = require ("../../../static/scripts/actions/actionCreators");
+const ACTION_TYPES = require ("../../../static/scripts/constants/actionTypes")
+
+describe ("action creators", () => {
+  it ("should create an action to update the view", () => {
+    const type = ACTION_TYPES.UPDATE_ACTIVE_VIEW;
+    const view = "DUMMY_VIEW";
+
+    const expectedAction = {
+      type,
+      view
+    }
+    expect (actionCreators.updateActiveView (view)).toEqual (expectedAction);
+  });
+
+  it ("should create an action to toggle agent typing", () => {
+    const type = ACTION_TYPES.TOGGLE_AGENT_TYPING;
+    const agentIsTyping = false;
+
+    const expectedAction = {
+      type,
+      typing: agentIsTyping
+    };
+
+    expect (actionCreators.toggleAgentTyping (agentIsTyping)).toEqual (expectedAction);
+  });
+});
