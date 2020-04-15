2630bb2aab3af5b851e9fe1219cfdcba214d9a05
Prasenjit Sharan <prasenjit@helpshift.com>
2020-04-15 22:51:03 +0530

    Add a DOMPurify hook to make all links open a new window
    Change-Id: I48b2a9e6bec1cc4066039b8db7f8771a2f54422e
    JIRA: https://helpshift.atlassian.net/browse/HSBUG-436

--- resources/static/scripts/extras/globalEvents.js
+++ resources/static/scripts/extras/globalEvents.js
@@ -5,11 +5,12 @@
  * @created Oct 24, 2018
  */
 
-define("extras/globalEvents", ["store", "actions/appState", "actions/chatView"], function(
-  store,
-  appStateActions,
-  chatViewActions
-) {
+define("extras/globalEvents", [
+  "store",
+  "actions/appState",
+  "actions/chatView",
+  "domPurify"
+], function(store, appStateActions, chatViewActions, DOMPurify) {
   "use strict";
 
   const {dispatch} = store;
@@ -49,7 +50,31 @@ define("extras/globalEvents", ["store", "actions/appState", "actions/chatView"],
     _addBlurEventListener();
   };
 
+  /**
+   * By default, DOMPurify removes the target attribute from the <a> tags. Add a hook to make all
+   * links open a new window.
+   * Source - https://github.com/cure53/DOMPurify/blob/master/demos/hooks-target-blank-demo.html
+   */
+  const addDomPurifyTargetHook = () => {
+    DOMPurify.addHook("afterSanitizeAttributes", function(node) {
+      // set all elements owning target to target=_blank
+      if ("target" in node) {
+        node.setAttribute("target", "_blank");
+        // prevent https://www.owasp.org/index.php/Reverse_Tabnabbing
+        node.setAttribute("rel", "noopener noreferrer");
+      }
+      // set non-HTML/MathML links to xlink:show=new
+      if (
+        !node.hasAttribute("target") &&
+        (node.hasAttribute("xlink:href") || node.hasAttribute("href"))
+      ) {
+        node.setAttribute("xlink:show", "new");
+      }
+    });
+  };
+
   return {
-    addFocusAndBlurEventListener
+    addFocusAndBlurEventListener,
+    addDomPurifyTargetHook
   };
 });
