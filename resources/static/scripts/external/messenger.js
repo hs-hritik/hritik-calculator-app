96a4c7788b0bfd419edf5f831a33fc22ab6732e0
Prasenjit Sharan <prasenjit@helpshift.com>
2020-09-25 23:54:58 +0530

    Revert "Mark web chat as ready when iframe's JS is loaded"
    This reverts commit 23d5de904f0ce6239d74fc019265b0b9fe9c454f.
    
    Change-Id: Idc3ec11d8cf0ef614b2eab5bcd40fa4c95ef7cf0

--- resources/static/scripts/external/messenger.js
+++ resources/static/scripts/external/messenger.js
@@ -849,6 +849,11 @@
     updateIframeStyles(config);
 
     const launcherHidden = !state.widgetOptions.showLauncher;
+    // If the launcher iframe is hidden by the widget config options
+    // then mark sdk as ready
+    if (launcherHidden) {
+      markSdkReady();
+    }
 
     // If launcher is hidden or launcher iframe is already created then
     // don't create launcherIframe
@@ -900,6 +905,8 @@
       });
 
       launcherIframe.contentDocument.body.appendChild(launcherBtn);
+
+      markSdkReady();
 
       // If widgetShouldAutoOpen is true then dispatch message to open
       // the widget.
@@ -1195,12 +1202,10 @@
             // config, which along with other settings, determines whether
             // the widget should load or not.
 
-            // Mark SDK as ready so that queued events can be flushed.
             // Pass client config and parent page info to set initial app data.
             // Also, pass the localStorage data to migrate. Passing this with setConfig
             // in order to avoid another asynchronous postMessage call to the web
             // chat iframe.
-            markSdkReady();
             setConfig({
               clientConfig: win.helpshiftConfig,
               parentPageInfo
