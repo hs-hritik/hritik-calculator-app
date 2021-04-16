ef148c3dc6ef872b1a38e67c340b3aa528f44513
Prasenjit Sharan <prasenjit@helpshift.com>
2021-04-16 13:49:00 +0530

    Log helpshiftConfig object for feature analysis
    Fire a non-existent xhr with helpshiftConfig as a param in order to
    analyze it for a feature.
    
    Change-Id: I048af82c97f56860ee6e6b816573b4f59807a792

--- resources/static/scripts/helpers/localStorage.js
+++ resources/static/scripts/helpers/localStorage.js
@@ -29,7 +29,8 @@ define("helpers/localStorage", [
     PFI_VALUE: "pfi",
     CONFIG: "config",
     RESPECT_PFI: "rf",
-    WS_CONFIG: "wsc"
+    WS_CONFIG: "wsc",
+    HS_CONFIG_LOGGED: "hcl"
   };
 
   const PROACTIVE_CHAT_KEYS = ["SITE_ACTIVITY_START_TIME", "PROACTIVE_CHAT_HAS_TRIGGERED"];
